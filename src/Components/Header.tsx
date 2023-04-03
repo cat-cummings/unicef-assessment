d0d87a3ac83bbb0b8af26cf2811525aa231830b1
Catlin Cummings <catlincummings@Catlins-MacBook-Pro.local>
2023-04-03 14:16:16 +1200

    Added navigation to cart and back to list

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import { useLocation, Link } from "react-router-dom"
 
 function Header() {
+    const location = useLocation()
+
     return(
-        <div>
-            <h1>Welcome to the Shop!</h1>
-            <p></p>
+        <div className='header'>
+            <h1 className="welcome">Welcome to the Shop!</h1>
+            <p className='nav-button'>{location.pathname === '/' ? <Link to='/cart'>Cart</Link> : <Link to='/'>Back</Link>}</p>
         </div>
     )
 }
